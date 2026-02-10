b2634670340e4e551f8e795586300e700667843e
kenobijr <kenobijr@tatooine.com>
2026-02-10 17:42:48 +0100

    completed base version of data-prep pipeline orchestrator

--- forge.py
+++ forge.py
@@ -1,13 +1,19 @@
 """
 - input book path .md with content sep by # Chapter 1, ... anchors
-- 0-n optional reference input path; if more than 1 ref input path: 
+- 0-n optional reference input path; if more than 1 ref input path:
     - ref .md files must be pre-split to fit scene size
-    - ref .md files must be passed in meaningful consecutive order 
+    - ref .md files must be passed in meaningful consecutive order
 """
 
 import argparse
 from typing import Tuple
 from src.book_creator import BookProcessor
+from src.scene_creator import SceneProcessor
+from src.manual_scene_adder import add_scene
+from src.summary_creator import SummaryProcessor
+from src.remove_ref_scenes import remove_ref_scenes
+from src.instruction_creator import InstructionProcessor
+from src.dataset_compiler import CompileProcessor
 
 
 def construct_ref(input_ref_path: Tuple[str]):
@@ -21,14 +27,44 @@ def construct_ref(input_ref_path: Tuple[str]):
 
 
 def forge_book(input_book_path: str, *input_ref_path: str):
+    print(f"FORGER_start: process book {input_book_path} ...")
+    print("------Stage 1: Book & World Context-------")
+    # set flag if ref mat provided; default case: no; if yes, custom logic needed at some stages
+    has_ref = False
     # if no input_ref_path provided set param to None; else construct it depending on amount
     if not input_ref_path:
         ref = None
     else:
+        has_ref = True
         ref = construct_ref(input_ref_path)
     # init base book .json & world_context creation -> target dir: json/base
-    # b = BookProcessor(input_book_path, ref)
-    # b.run()
+    b = BookProcessor(input_book_path, ref)
+    base_path = b.run()
+    print("------Stage 2: Semantic Scene-------")
+    # process narrative chapters into semantic scenes; custom logic for ref material
+    s = SceneProcessor(base_path)
+    scene_path = s.run()
+    # if ref provided, prepend this scenes manually to scenes list
+    if has_ref:
+        for ref_path in input_ref_path:
+            add_scene(scene_path, ref_path)
+    print("------Stage 3: Running Summary-------")
+    # create running summaries along semantic (ref) scenes as timesteps
+    sp = SummaryProcessor(scene_path)
+    summary_path = sp.run()
+    # if ref provided, remove ref scenes here (were compressed to running sum of 1st semantic scene)
+    if has_ref:
+        remove_ref_scenes(summary_path)
+    # create custom instruction along every semantic scene
+    print("------Stage 4: Instruction Tuning-------")
+    i = InstructionProcessor(summary_path)
+    instruction_path = i.run()
+    print("------Stage 5: Final .jsonl-------")
+    # create target .jsonl output
+    c = CompileProcessor(instruction_path)
+    final_output_path = c.run()
+    print(f"Ops completed: target .jsonl file at {final_output_path}")
+    print("FORGER_end: Operation completed successfully-------")
 
 
 def main():
@@ -44,6 +80,7 @@ def main():
     )
     args = parser.parse_args()
     forge_book(args.input_book_path, *args.input_ref_path)
+
 
 if __name__ == "__main__":
     main()
